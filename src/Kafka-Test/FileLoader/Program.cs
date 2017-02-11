7846c54749d376ec68840b500b114105839d4b0f
Jamie <Jamie.Wroe@live.co.uk>
2017-02-11 18:56:22 +0000

    In a coninuing effort to try out things I've read, putting unit tests with code under test

---
+++ src/Kafka-Test/FileLoader/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the path of your file to process...");
+                var path = Console.ReadLine();
+                var uncleanFile = new UncleanFile(new FileOnDisk(path));
+            }
+
+        }
+    }
+
+    internal class FileOnDisk
+    {
+        private readonly string _path;
+
+        public FileOnDisk(string path)
+        {
+            _path = path;
+            
+        }
+    }
+}
